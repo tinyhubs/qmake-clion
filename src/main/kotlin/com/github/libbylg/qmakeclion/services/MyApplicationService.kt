3f9fbe71ad0025cda1c517fcc035a839432b85a3
GitHub Action <action@github.com>
2021-02-11 01:46:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/libbylg/qmakeclion/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/libbylg/qmakeclion/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.libbylg.qmakeclion.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.libbylg.qmakeclion.MyBundle
 
 class MyApplicationService {
 
